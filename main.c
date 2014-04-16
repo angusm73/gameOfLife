5740bf0a203b0a71aef22016aecf1e38226d09e9
Angus Moore <toxic0323@gmail.com>
2014-04-16 21:45:58 +1000

    Fixed bug where it wouldn't update properly, just added a temp array to fix.

--- main.c
+++ main.c
@@ -1,13 +1,20 @@
+// Conway's Game of Life in C
+// (c)oded by Angus Moore
+// email: toxic0323@gmail.com
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
 
 #define ON 219
-#define OFF 0
+#define OFF 176
+#define GRID_WIDTH 100
+#define GRID_HEIGHT 50
+#define GRID_SIZE GRID_WIDTH*GRID_HEIGHT
 
 typedef int State;
-typedef int Grid [600];
+typedef int Grid[GRID_SIZE];
 typedef int Cell;
 
 void sleep(time_t delay) {
@@ -20,7 +27,7 @@ void sleep(time_t delay) {
 
 void initGrid(Grid grid) {
     int x;
-    for (x = 1; x < 600; x++) {
+    for (x = 1; x < GRID_SIZE; x++) {
         int r = rand() % 2;
         if (r == 1) {
             grid[x] = ON;
@@ -30,15 +37,47 @@ void initGrid(Grid grid) {
     }
 }
 
+void initGlider(Grid grid) {
+    int x;
+    for (x = 1; x < GRID_SIZE; x++) {
+        if (x == 2 || x == 103 || x == 201 || x == 202 || x == 203) {
+            grid[x] = ON;
+        } else {
+            grid[x] = OFF;
+        }
+    }
+}
+
+void initTest(Grid grid) {
+    int x;
+    for (x = 1; x < GRID_SIZE; x++) {
+        if (x == 122 || x == 223 || x == 321 || x == 322 || x == 323) {
+            grid[x] = ON;
+        } else {
+            grid[x] = OFF;
+        }
+    }
+}
+
+void clearGrid(Grid grid) {
+    int x;
+    for (x = 0; x < GRID_SIZE; x++) {
+        grid[x] = OFF;
+    }
+}
+
 int getNeighbours(Grid grid, Cell cell) {
     int neighbours = 0;
-    if (grid[cell - 41] == ON) {
+
+//    printf("%i", grid[cell] % GRID_WIDTH);
+
+    if (grid[cell - 101] == ON) {
         neighbours += 1;
     }
-    if (grid[cell - 40] == ON) {
+    if (grid[cell - 100] == ON) {
         neighbours += 1;
     }
-    if (grid[cell - 39] == ON) {
+    if (grid[cell - 99] == ON) {
         neighbours += 1;
     }
     if (grid[cell - 1] == ON) {
@@ -47,19 +86,19 @@ int getNeighbours(Grid grid, Cell cell) {
     if (grid[cell + 1] == ON) {
         neighbours += 1;
     }
-    if (grid[cell + 39] == ON) {
+    if (grid[cell + 99] == ON) {
         neighbours += 1;
     }
-    if (grid[cell + 40] == ON) {
+    if (grid[cell + 100] == ON) {
         neighbours += 1;
     }
-    if (grid[cell + 41] == ON) {
+    if (grid[cell + 101] == ON) {
         neighbours += 1;
     }
     return neighbours;
 }
 
-void updateCell(Grid grid, Cell cell) {
+void updateCell(Grid grid, Grid temp, Cell cell) {
 //    int r = rand() % 2;
 //    if (r == 1) {
 //        grid[cell] = ON;
@@ -68,50 +107,49 @@ void updateCell(Grid grid, Cell cell) {
 //    }
     int p = getNeighbours(grid, cell);
     if (grid[cell] == ON && p < 2) {
-        grid[cell] = OFF;
+        temp[cell] = OFF;
     } else if (grid[cell] == ON && p > 3) {
-        grid[cell] = OFF;
+        temp[cell] = OFF;
     } else if (grid[cell] == OFF && p == 3) {
-        grid[cell] = ON;
+        temp[cell] = ON;
     }
 }
 
-void updateGrid(Grid grid) {
-    int x;
-    for (x = 1; x < 600; x++) {
-        int r = rand() % 2;
-        if (r == 1) {
-            grid[x] = ON;
-        } else {
-            grid[x] = 176;
-        }
-        updateCell(grid, grid[x]);
-    }
+void updateGrid(Grid grid, Grid temp) {
+    grid = temp;
 }
 
 void displayGrid(Grid grid) {
     int x;
-    for (x = 1; x < 600; x++) {
-        if (x % 40) {
-            printf("%c", grid[x]);
-        } else {
+    for (x = 1; x < GRID_SIZE; x++) {
+        if (x % GRID_WIDTH == 0) {
             printf("\n");
+        } else {
+            printf("%c", grid[x]);
         }
     }
 }
 
 int main() {
     Grid grid;
+    Grid temp;
     initGrid(grid);
+    // Change initGrid to initGlider for a glider that travels from the top left to the bottom right.
+    int p;
+    for (p = 0; p < GRID_SIZE; p++) {
+        temp[p] = grid[p];
+    }
     while (1) {
-        int x;
-        for (x = 1; x < 600; x ++) {
-            updateCell(grid, x);
+        for (p = 0; p < GRID_SIZE; p++) {
+            grid[p] = temp[p];
         }
-        displayGrid(grid);
-        int count;
-        time_t start, finish;
-        sleep(1);
+        int x;
+        displayGrid(temp);
+//        sleep(1);
+        for (x = 1; x < GRID_SIZE; x ++) {
+            updateCell(grid, temp, x);
+        }
+        updateGrid(grid, temp);
         system("cls");
     }
     return 0;
